4c300b7a687024cf0723b57d09cb68e7bb3ba5ab
Robert Ludwig <robert@ludwig-jahn.com>
2020-03-15 11:14:38 +0100

    Create test class for bot methods

---
+++ src/BotTest.py
@@ -0,0 +1,13 @@
+import unittest
+from GameWindow import Window
+
+
+class MyTestCase(unittest.TestCase):
+
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()

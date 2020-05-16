2a1cddc93846d2d6b7738ee1b8c5ce98c7266261
Robert Ludwig <robert@ludwig-jahn.com>
2020-05-16 20:02:33 +0200

    Fix and optimice a* bot

--- src/SnakeBot.py
+++ src/SnakeBot.py
@@ -7,6 +7,8 @@ class SnakeBot:
         self.snake = snake
         # idle bot path
         self.idle_path = []
+        # counts idle steps till recalculation of idle_path
+        self.idle_count = 0
         # idle bot counter direction
         self.tup_dir = {0: (0, 2), 1: (1, 0), 2: (0, -2), 3: (-1, 0)}
         # the path the bot has found
@@ -59,8 +61,7 @@ class SnakeBot:
                             step_dict[step].add(neighbor)
             # if no path to the target was found bot needs to idle
             if step >= 150:
-                self.idle_bot()
-                return
+                return self.idle_bot()
             # if the target is in the reachable fields the loop stops
             if (food_y, food_x) in step_dict[step]:
                 target_reached = True
@@ -69,6 +70,7 @@ class SnakeBot:
                 step += 1
 
         self.idle_path.clear()
+        self.idle_count = 0
         self.bot_path.clear()
         # saves the path to the target
         self.bot_path = [(food_y, food_x)]
@@ -86,124 +88,117 @@ class SnakeBot:
             self.bot_path.insert(0, candidate)
             step -= 1
         # checks if the snake will run into a dead end and returns the next tup
-        next_tup = self.dead_end_check(self.bot_path[0], (cur_y, cur_x), self.snake.tabu_fields)
+        next_tup = self.dead_end_check(self.bot_path[0], (cur_y, cur_x), future_tabu_fields)
         # translates the next field, to go to, into a direction
         next_direction = self.tup_to_direction(self.snake.direction, (cur_y, cur_x), next_tup)
         return next_direction
 
     def idle_bot(self):
         head_tup = self.snake.head.get_coordinates()
+        if self.idle_count < 5 and len(self.idle_path) > 0:
+            self.idle_count = 1
+            next_tup = self.idle_path.pop(0)
+            next_direction = self.tup_to_direction(self.snake.direction, head_tup, next_tup)
+            return next_direction
+
         tabu_fields = self.snake.tabu_fields.copy()
-        if len(self.idle_path) == 0:
-            # saves the 4 idle paths
-            results = []
+        # saves the 4 idle paths
+        results = []
 
-            for i in range(4):
-                # safes the current path
-                cur_list = []
-                # index for the offset dict
-                tup_dict_index = (i - 1) % 4
-                # alternates between -1, 0, 1 and determines the cur_dir_index
-                offset_pointer = -1
-                # shows in witch direction the offset_pointer evolves
-                offset_dir_flag = True
-                # is the field the algorithem is currently at
-                work_tup = head_tup
-                # counts the steps to prevent that the bot loops across the whole game win
-                step_counter = 0
-                if tuple(map(self.add_tup, work_tup, self.tup_dir[tup_dict_index])) in tabu_fields:
-                    tup_dict_index = (tup_dict_index + 2) % 4
-                    offset_pointer = 1
-                    offset_dir_flag = False
+        for i in range(4):
+            # safes the current path
+            cur_list = []
+            # index for the offset dict
+            tup_dict_index = (i - 1) % 4
+            # alternates between -1, 0, 1 and determines the cur_dir_index
+            offset_pointer = -1
+            # shows in witch direction the offset_pointer evolves
+            offset_dir_flag = True
+            # is the field the algorithem is currently at
+            work_tup = head_tup
+            # counts the steps to prevent that the bot loops across the whole game win
+            step_counter = 0
+            if tuple(map(self.add_tup, work_tup, self.tup_dir[tup_dict_index])) in tabu_fields:
+                tup_dict_index = (tup_dict_index + 2) % 4
+                offset_pointer = 1
+                offset_dir_flag = False
 
-                fields_left = True
-                while fields_left:
-                    if offset_pointer != 0:
-                        step_counter += 1
-                        next_field = tuple(map(self.add_tup, work_tup, self.tup_dir[tup_dict_index]))
-                        if next_field in tabu_fields or \
-                                tuple(map(self.add_tup, next_field, self.tup_dir[i])) in tabu_fields or \
-                                ((i == 0 or i == 2) and step_counter >= 13) or \
-                                ((i == 1 or i == 3) and step_counter >= 27):
-                            step_counter = 0
-                            tup_dict_index = i
-                            if offset_pointer == -1:
-                                offset_dir_flag = True
-                            else:
-                                offset_dir_flag = False
-                            offset_pointer = 0
+            fields_left = True
+            while fields_left:
+                if offset_pointer != 0:
+                    step_counter += 1
+                    next_field = tuple(map(self.add_tup, work_tup, self.tup_dir[tup_dict_index]))
+                    if next_field in tabu_fields or \
+                            tuple(map(self.add_tup, next_field, self.tup_dir[i])) in tabu_fields or \
+                            ((i == 0 or i == 2) and step_counter >= 13) or \
+                            ((i == 1 or i == 3) and step_counter >= 27):
+                        step_counter = 0
+                        tup_dict_index = i
+                        if offset_pointer == -1:
+                            offset_dir_flag = True
                         else:
-                            cur_list.append(next_field)
-                            work_tup = next_field
+                            offset_dir_flag = False
+                        offset_pointer = 0
                     else:
-                        next_field = tuple(map(self.add_tup, work_tup, self.tup_dir[tup_dict_index]))
-                        if next_field in tabu_fields:
-                            fields_left = False
+                        cur_list.append(next_field)
+                        work_tup = next_field
+                else:
+                    next_field = tuple(map(self.add_tup, work_tup, self.tup_dir[tup_dict_index]))
+                    if next_field in tabu_fields:
+                        fields_left = False
+                    else:
+                        if offset_dir_flag:
+                            offset_pointer += 1
+                            # distance_buffer = (2, 2)
                         else:
-                            if offset_dir_flag:
-                                offset_pointer += 1
-                                # distance_buffer = (2, 2)
-                            else:
-                                offset_pointer -= 1
-                                # distance_buffer = (1, 1)
-                            tup_dict_index = (tup_dict_index + offset_pointer) % 4
-                            cur_list.append(next_field)
-                            work_tup = next_field
+                            offset_pointer -= 1
+                            # distance_buffer = (1, 1)
+                        tup_dict_index = (tup_dict_index + offset_pointer) % 4
+                        cur_list.append(next_field)
+                        work_tup = next_field
 
-                results.append(cur_list)
+            results.append(cur_list)
 
-            # calculate the longest idle path
-            best_path = []
-            for path in results:
-                if len(path) > len(best_path):
-                    best_path = path
+        # calculate the longest idle path
 
-            if len(best_path) == 0:
-                # snake just goes ahead
-                return self.snake.direction
+        for path in results:
+            if len(path) > len(self.idle_path):
+                self.idle_path = path
 
-            if len(best_path) > len(self.idle_path):
-                self.idle_path = best_path
+        if len(self.idle_path) == 0:
+            # snake just goes ahead
+            return self.snake.direction
 
         next_tup = self.idle_path.pop(0)
         next_direction = self.tup_to_direction(self.snake.direction, head_tup, next_tup)
+        self.idle_count += 1
         return next_direction
 
     def dead_end_check(self, next_tup, head_tup, tabu_fields):
-        check_tups = self.neighbors(next_tup)
-        tabu_count = 0
-        for tup in check_tups:
-            if tup in tabu_fields or tup == head_tup:
-                tabu_count += 1
-        if tabu_count >= 2:
-            next_tup_count = self.flood_fill_counter(next_tup, tabu_fields)
-            if next_tup_count >= (len(self.snake.snake_fields)):
-                return next_tup
-            choices_list = [i for i in self.neighbors(head_tup) if i not in tabu_fields and i != next_tup]
-            best_choice = (next_tup, next_tup_count)
-            for choice in choices_list:
-                field_count = self.flood_fill_counter(choice, tabu_fields)
-                if field_count > best_choice[1]:
-                    best_choice = (choice, field_count)
-            return best_choice[0]
-        return next_tup
+        tabu_fields.add(head_tup)
+        next_tup_count = self.flood_fill_counter(next_tup, tabu_fields)
+        if next_tup_count >= (len(self.snake.snake_fields)):
+            return next_tup
+        best_choice = (next_tup, next_tup_count)
+        choices_list = [i for i in self.neighbors(head_tup) if i not in tabu_fields and i != next_tup]
+        for choice in choices_list:
+            field_count = self.flood_fill_counter(choice, tabu_fields)
+            if field_count > best_choice[1]:
+                best_choice = (choice, field_count)
+        return best_choice[0]
 
     @staticmethod
     def flood_fill_counter(start_tup, tabu_fields):
-        field_set = {start_tup}
+        field_set = set()
         work_list = [start_tup]
-        while True:
-            temp_list = []
-            for cur_tup in work_list:
-                work_list.remove(cur_tup)
-                check_tups = SnakeBot.neighbors(cur_tup)
-                for tup in check_tups:
-                    if tup not in tabu_fields and tup not in field_set:
-                        field_set.add(tup)
-                        temp_list.append(tup)
-            if len(temp_list) == 0:
-                break
-            work_list = temp_list
+        while len(work_list) > 0:
+            # temp_list = []
+            cur_tup = work_list.pop()
+            if cur_tup in tabu_fields or cur_tup in field_set:
+                continue
+            else:
+                work_list.extend(SnakeBot.neighbors(cur_tup))
+                field_set.add(cur_tup)
         return len(field_set)
 
     @staticmethod
